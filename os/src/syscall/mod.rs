003e594eff013d9190c38af8d32fd1d7c59d69a9
LiuZhetan <liuzhetande@foxmail.com>
2023-10-24 17:45:07 +0800

    pass ch3 test

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -37,3 +37,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
+
+
+// Add pub use
+pub use process::TaskInfo;
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,6 +1,7 @@
 //! Types related to task management
 
 use super::TaskContext;
+use crate::config::MAX_SYSCALL_NUM;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
@@ -9,6 +10,22 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// Add the syscall times
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
+    /// Add The time process start to run, the unit is microsecond
+    pub start_time: usize
+}
+
+impl TaskControlBlock {
+    /// Create TaskControlBlock by given TaskStatus and TaskContext
+    pub fn new(task_status: TaskStatus, task_cx: TaskContext) -> Self {
+        TaskControlBlock {
+            task_status,
+            task_cx,
+            syscall_times: [0u32; MAX_SYSCALL_NUM],
+            start_time: 0
+        }
+    }
 }
 
 /// The status of a task
